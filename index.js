3ee329e9becc6a88828d30057fce283f9e143ddb
DarwinLozada <darwinlozadadev@gmail.com>
2021-02-07 01:23:05 -0400

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
-const PORT = 3001;
+const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 const unknownEndPoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
@@ -12,16 +14,8 @@ morgan.token("data", (req, res) => {
   return JSON.stringify(req.body);
 });
 
-const requestLogger = (req, res, next) => {
-  console.log("Method:", req.method);
-  console.log("Path:  ", req.path);
-  console.log("Body:  ", req.body);
-  console.log("---------");
-  next();
-};
-
 app.use(express.json());
-// app.use(requestLogger);
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(morgan(":data"));
 
